dd3a649435da9fba95383ed7f8a9051f0c36d419
Xu Xinran <xxr3376@gmail.com>
2014-08-18 12:18:40 +0800

    refract downloader using util.retry decorator, leave a resource interface

--- agent/downloader.py
+++ agent/downloader.py
@@ -1,7 +1,33 @@
 import requests
 import util
+import resource_manager
+from requests.exceptions import ConnectionError, HTTPError, TooManyRedirects, Timeout
+import const
+from util import retry
+import random
+import time
 
 def basic_downloader(query, base_headers, timeout):
+    result = __basic_downloader(query, base_headers, timeout)
+    answer = generate_answer(result)
+    return answer
+
+def _exception(e, query, base_headers, timeout):
+    print query['url']
+    print e
+    time.sleep(random.random() * 5)
+    return
+
+def _fatal(e, query, base_headers, timeout):
+    return {
+        'query': query,
+        'state': 'fail',
+        'exception': e,
+    }
+    return e
+
+@retry(times=const.RETRY_CNT_PER_URL, except_callback=_exception, fatal_callback=_fatal)
+def __basic_downloader(query, base_headers, timeout):
     s = requests.session()
     url = query['url']
     arguments = {
@@ -10,15 +36,54 @@ def basic_downloader(query, base_headers, timeout):
         "allow_redirects": True,
     }
     if query.get('need_login', False):
-        resource_id = query['resource']
-        cookies = {} #TODO
+        resource_name = query['resource']
+        cookies = resource_manager.get_resource(resource_name)
         arguments['cookies'] = cookies
     redirect = True
     while redirect:
         r = s.get(url, **arguments)
         redirect, url = util.test_for_meta_redirections(r)
         r.raise_for_status()
-    return r
+    return {
+        'query': query,
+        'state': 'ok',
+        'response': r,
+    }
+
+def judge_fail_reason(exception):
+    fail_reason_map = {
+        ConnectionError: const.FAIL_REASON['ConnectionError'],
+        HTTPError: const.FAIL_REASON['HTTPError'],
+        Timeout: const.FAIL_REASON['Timeout'],
+        TooManyRedirects: const.FAIL_REASON['TooManyRedirects'],
+    }
+    return fail_reason_map.get(type(exception), const.FAIL_REASON['Other'])
+
+def generate_answer(result):
+    answer = {
+        'url': result['query']['url']
+    }
+    if result['state'] == 'ok':
+        r = result['response']
+        extension = util.guess_extension(r)
+        if extension in const.VALID_EXTENSION:
+            answer['success'] = True
+            answer['content'] = r.text
+            answer['headers'] = {}
+            answer['final_url'] = r.url
+            for field in const.INTEREST_HEADER_FIELDS:
+                if field in r.headers:
+                    answer['headers'][field] = r.headers[field]
+            answer['status_code'] = r.status_code
+        else:
+            answer['success'] = False
+            answer['raw_exception_text'] = 'Wrong extension, this is %s' % extension
+            answer['fail_reason'] = const.FAIL_REASON['WrongExtension']
+    else:
+        answer['success'] = False
+        answer['raw_exception_text'] = str(result['exception'])
+        answer['fail_reason'] = judge_fail_reason(result['exception'])
+    return answer
 
 downloader_config = {
     'basic': basic_downloader,
--- agent/worker.py
+++ agent/worker.py
@@ -1,12 +1,9 @@
 #!encoding=utf-8
-import requests
 import threading
-import util
 import const
-import random
-import time
-from requests.exceptions import ConnectionError, HTTPError, TooManyRedirects, Timeout
 from downloader import downloader_config
+import time
+import random
 
 class Worker(threading.Thread):
     def __init__(self, query_list, timeout=const.READ_TIMEOUT_LIMIT):
@@ -19,79 +16,17 @@ class Worker(threading.Thread):
     def run(self):
         headers = self.generate_header()
         for query in self.query_list:
-            result = self.crawl_query(query, headers)
-            answer = self.generate_answer(result)
+            answer = self.crawl_query(query, headers)
             self.answers.append(answer)
             time.sleep(random.random() * 5)
         return
 
-    def judge_fail_reason(self, exception):
-        fail_reason_map = {
-            ConnectionError: const.FAIL_REASON['ConnectionError'],
-            HTTPError: const.FAIL_REASON['HTTPError'],
-            Timeout: const.FAIL_REASON['Timeout'],
-            TooManyRedirects: const.FAIL_REASON['TooManyRedirects'],
-        }
-        return fail_reason_map.get(type(exception), const.FAIL_REASON['Other'])
-
-    def generate_answer(self, result):
-        answer = {
-            'url': result['query']['url']
-        }
-        if result['state'] == 'ok':
-            r = result['response']
-            extension = util.guess_extension(r)
-            if extension in const.VALID_EXTENSION:
-                answer['success'] = True
-                answer['content'] = r.text
-                answer['headers'] = {}
-                answer['final_url'] = r.url
-                for field in const.INTEREST_HEADER_FIELDS:
-                    if field in r.headers:
-                        answer['headers'][field] = r.headers[field]
-                answer['status_code'] = r.status_code
-            else:
-                answer['success'] = False
-                answer['raw_exception_text'] = 'Wrong extension, this is %s' % extension
-                answer['fail_reason'] = const.FAIL_REASON['WrongExtension']
-        else:
-            answer['success'] = False
-            answer['raw_exception_text'] = str(result['exception'])
-            answer['fail_reason'] = self.judge_fail_reason(result['exception'])
-        return answer
-
     def crawl_query(self, query, headers):
-        retry_cnt = 0
-        # easy access for get data
-
-        result = {}
-
-        result['query'] = query
         downloader_type = query.get('downloader', 'basic')
         downloader = downloader_config[downloader_type]
         #each url will retry for N times
-        while True:
-            try:
-                r = downloader(query, headers, self.timeout)
-                # already get final html
-                result['state'] = 'ok'
-                result['response'] = r
-                # done for crawl, exist repeat
-                break
-            except (KeyboardInterrupt, SystemExit):
-                raise
-            except Exception as e:
-                print query['url']
-                print e
-                retry_cnt += 1
-
-                if retry_cnt >= const.RETRY_CNT_PER_URL:
-                    result['state'] = 'fail'
-                    result['exception'] = e
-                    break
-                else:
-                    time.sleep(random.random() * 5)
-        return result
+        answer = downloader(query, headers, self.timeout)
+        return answer
 
     def generate_header(self):
         headers = {
